cebbc4d26e897ab7a7d1868cc9a49de93062769c
Carla Vidales <carlavidales.ca@gmail.com>
2023-12-05 13:46:10 -0800

    Test calling API from React

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [courses, setCourses] = useState([]);
+  const apiUrl = "http://localhost:5000/api/courses/";
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      setCourses(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const generateCourseList = () => {
+    return (
+      <div id="courseListContainer">
+        <div id="courseList">
+          <ul>
+            {courses.map((course) => (
+              <li key={course.id}>{course.title}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Course List</h1>
+      {generateCourseList()}
     </div>
   );
 }
